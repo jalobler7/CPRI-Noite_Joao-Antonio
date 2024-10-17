15b33e6401d33c01dca675e085298ee94af2e24d
jalobler7 <jablobler@inf.ufsm.br>
2024-10-17 20:26:23 -0300

    Templates Iniciais

--- cppri_projetofinal/settings.py
+++ cppri_projetofinal/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 INSTALLED_APPS +=['home', ]
+INSTALLED_APPS +=['django_bootstrap5', ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -105,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+#from tempfile import template
 
-def index(request):
-    return render(request, 'index.html')
+#from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name= 'index.html'

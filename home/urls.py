f505b967df30d575d5987c86a3fa8cc763073573
jalobler7 <jablobler@inf.ufsm.br>
2024-11-13 16:55:51 -0300

    atualizacoes de links

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,10 @@ from operator import index
 
 from django.contrib import admin
 from django.urls import path, include
-from .views  import IndexView
+from .views  import IndexView, render, TemplateView
+from . import views
 
 urlpatterns = [
     path('', IndexView.as_view(), name = 'index'),
+    path('', views.pagina_inicial, name='pagina_inicial')
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
 #from tempfile import template
-
+from django.shortcuts import render
 #from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name= 'principal.html'
+
+def pagina_inicial(request):
+    return render(request, 'principal.html')
